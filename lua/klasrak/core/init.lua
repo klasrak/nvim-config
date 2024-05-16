de538593e9242ac382645045fae3908e6cb57be8
Danilo Augusto <dasf_cm@terra.com.br>
2024-05-16 12:08:19 -0300

    feat: add keymaps

--- lua/klasrak/core/init.lua
+++ lua/klasrak/core/init.lua
@@ -1,1 +1,2 @@
 require("klasrak.core.options")
+require("klasrak.core.keymaps")
--- lua/klasrak/core/keymaps.lua
+++ lua/klasrak/core/keymaps.lua
@@ -1,1 +1,5 @@
 vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
