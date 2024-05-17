0460f062cf6ad2099c7d8929f4a68843f62ebfd0
Danilo Augusto <dasf_cm@terra.com.br>
2024-05-17 13:02:21 -0300

    refactor: disable lazy notification for new modules

--- lua/klasrak/lazy.lua
+++ lua/klasrak/lazy.lua
@@ -11,4 +11,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("klasrak.plugins")
+require("lazy").setup("klasrak.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
